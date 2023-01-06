f0da10d28aa519741d64ece5a523e1836673f8d2
Murat Başeren <uskudar.iskur.z36@gmail.com>
2023-01-06 10:52:17 +0300

    .

--- AdminLTETemplate/Controllers/HomeController.cs
+++ AdminLTETemplate/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace AdminLTETemplate.Controllers
             return View();
         }
 
+        public IActionResult table()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
